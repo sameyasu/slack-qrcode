56b2b67254e4cc8f45d173a3da7f298a5832102a
sameyasu <sameyasu@gmail.com>
2019-02-21 23:54:42 +0900

    Try to create example QRcode.

--- src/SlashCommand.php
+++ src/SlashCommand.php
@@ -1,14 +1,28 @@
 <?php
 namespace SlackQrcode;
 
+use chillerlan\QRCode\QRCode;
+use chillerlan\QRCode\QROptions;
+
 class SlashCommand
 {
     use Logger;
 
     public function qrcode()
     {
-        $this->log('this is a test');
-        $this->log()->info('this is a test');
-        $this->log()->warning('this is a warning');
+        $options = new QROptions([
+            'outputType'        => QRCode::OUTPUT_IMAGE_PNG,
+            'eccLevel'          => QRCode::ECC_L,
+            'imageBase64'       => false,
+            'imageTransparent'  => false,
+        ]);
+
+        $qrcode = new QRCode($options);
+
+        $data = 'https://github.com/';
+        $this->log()->debug('[data] ' . $data);
+
+        header('Content-Type: image/png');
+        echo $qrcode->render($data);
     }
 }
