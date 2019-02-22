92f9e4486977fde76a2b424326cbe315353bc0fa
sameyasu <sameyasu@gmail.com>
2019-02-23 00:37:45 +0900

    Fix SlashCommand to create url.

--- src/SlashCommand.php
+++ src/SlashCommand.php
@@ -8,21 +8,38 @@ class SlashCommand
 {
     use Logger;
 
-    public function qrcode()
+    public function qrcode(array $params)
     {
-        $options = new QROptions([
-            'outputType'        => QRCode::OUTPUT_IMAGE_PNG,
-            'eccLevel'          => QRCode::ECC_L,
-            'imageBase64'       => false,
-            'imageTransparent'  => false,
-        ]);
+        $token = $params['token'] ?? '';
+        if (!$this->isValidToken($token)) {
+            $this->log('Invalid token: ' . $token);
+            $this->respondError('Invalid Request');
+            return;
+        }
 
-        $qrcode = new QRCode($options);
+        echo $this->getQrcodeRenderURL($params['text']);
+    }
 
-        $data = 'https://github.com/';
-        $this->log()->debug('[data] ' . $data);
+    protected function getQrcodeRenderURL(string $text) : string
+    {
+        return sprintf('https://%s/img/%s', $_SERVER['HTTP_HOST'], urlencode($text));
+    }
 
-        header('Content-Type: image/png');
-        echo $qrcode->render($data);
+    protected function isValidToken(string $token) : bool
+    {
+        $valid = getenv('SLASH_COMMAND_TOKEN');
+        if (!empty($valid)) {
+            $this->log()->error('Not set Variable... SLASH_COMMAND_TOKEN');
+
+            return false;
+        }
+
+        return ($token === $valid);
+    }
+
+    protected function respondError(string $message)
+    {
+        header('Content-Type: text/plain; charset=UTF-8');
+        echo $message;
     }
 }
--- web/slash-command.php
+++ web/slash-command.php
@@ -4,5 +4,7 @@ require('../vendor/autoload.php');
 
 use \SlackQrcode\SlashCommand;
 
+$params = (isset($_GET['token']) ? $_GET : $_POST);
+
 $SlashCommand = new SlashCommand();
-$SlashCommand->qrcode();
+$SlashCommand->qrcode($params);
